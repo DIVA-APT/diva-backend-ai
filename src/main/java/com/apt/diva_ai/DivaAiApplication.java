da9ef21679a662243cb157db7a26cdbcdf5a56cb
sanghyuk <ish775800@gmail.com>
2025-01-09 09:51:37 +0900

    init: 초기 설정

---
+++ src/main/java/com/apt/diva_ai/DivaAiApplication.java
@@ -0,0 +1,13 @@
+package com.apt.diva_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivaAiApplication.class, args);
+	}
+
+}
