414293748951d5ab0a9c6aac4c80d0efe05b7c26
sanghyuk <ish775800@gmail.com>
2025-01-16 15:34:08 +0900

    feat: 이미지 다운 및 변환 util

--- src/main/java/com/apt/diva_ai/domain/s3/util/ImageUtils.java
+++ src/main/java/com/apt/diva_ai/domain/s3/util/ImageUtils.java
@@ -3,10 +3,15 @@ package com.apt.diva_ai.domain.s3.util;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Base64;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
+@RequiredArgsConstructor
 public class ImageUtils {
+
+    private final RestTemplate restTemplate;
 
     /**
      * Base64 이미지를 바이너리(InputStream) 형태로 변환
@@ -25,6 +30,12 @@ public class ImageUtils {
      * @return InputStream
      */
     public InputStream bytesToInputStream(byte[] imageBytes) {
+        return new ByteArrayInputStream(imageBytes);
+    }
+
+    // url 이미지를 다운로드 하고 InputStream으로 변환
+    public InputStream downloadImageToInputStream(String url) {
+        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
         return new ByteArrayInputStream(imageBytes);
     }
 }
