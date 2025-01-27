29efaa59ea398f566dc86dd7271b2752f164dcb4
sanghyuk <ish775800@gmail.com>
2025-01-27 22:05:41 +0900

    feat: 지표 카테고리 enum

---
+++ src/main/java/com/apt/diva_ai/global/enums/Category.java
@@ -0,0 +1,14 @@
+package com.apt.diva_ai.global.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Category {
+
+    EXPERT_ANALYSIS("전문가 분석"), FINANCIAL("재무제표"), MACROECONOMICS("거시경제"), INVESTMENT_MOVEMENT(
+        "투자동향"), NEWS("뉴스");
+
+    private final String title;
+}
