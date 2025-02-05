a4f20b02c68fa31a8cab21a36a2c0c06e943047c
sanghyuk <ish775800@gmail.com>
2025-02-05 21:15:24 +0900

    fix: DTO 필드명 수정

--- src/main/java/com/apt/diva_ai/domain/inference/dto/ChatBotResponseDTO.java
+++ src/main/java/com/apt/diva_ai/domain/inference/dto/ChatBotResponseDTO.java
@@ -1,10 +1,17 @@
 package com.apt.diva_ai.domain.inference.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class ChatBotResponseDTO {
+
+    @JsonAlias({"botMessage", "bot_message"})
     private String botMessage;
 }
--- src/main/java/com/apt/diva_ai/domain/inference/dto/ScriptResponseDTO.java
+++ src/main/java/com/apt/diva_ai/domain/inference/dto/ScriptResponseDTO.java
@@ -1,14 +1,16 @@
 package com.apt.diva_ai.domain.inference.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ScriptResponseDTO {
-    private String financial;
-    private String news;
-    private String analysis;
-    private String economy;
+    private String resultFin;
+    private String resultReport;
+    private String resultNews;
     private ReportDTO report;
 }
