20970cf3cb33a6ff96ee142de8c0ef745a1d4c19
sanghyuk <ish775800@gmail.com>
2025-02-06 16:20:36 +0900

    feat: ScriptDTO 개선

--- src/main/java/com/apt/diva_ai/domain/inference/dto/ScriptResponseDTO.java
+++ src/main/java/com/apt/diva_ai/domain/inference/dto/ScriptResponseDTO.java
@@ -1,16 +1,48 @@
 package com.apt.diva_ai.domain.inference.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.List;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ScriptResponseDTO {
-    private String resultFin;
-    private String resultReport;
-    private String resultNews;
-    private ReportDTO report;
+
+    private ResultFin resultFin;
+    private ResultReport resultReport;
+    private ResultNews resultNews;
+
+    @JsonAlias({"botMessage", "bot_message"})
+    private String botMessage;
+
+    @Data
+    static class ResultFin {
+
+        private String resultFin;
+        private List<ReferenceLinks> referenceLinks;
+    }
+
+    @Data
+    static class ResultReport {
+
+        private String resultReport;
+        private List<ReferenceLinks> referenceLinks;
+    }
+
+    @Data
+    static class ResultNews {
+
+        private String resultNews;
+        private List<ReferenceLinks> referenceLinks;
+    }
+
+    @Data
+    static class ReferenceLinks {
+
+        private String title;
+        private String description;
+        private String url;
+    }
 }
